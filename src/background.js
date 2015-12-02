92081fb5c3e22ad95c5281132edca2a74b7942b9
Terry Sun <terrynsun@gmail.com>
2015-12-02 11:22:52 -0500

    Remove popup and print statements..

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@ var connections = {};
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
  //  console.log('incoming message from injected script');
- console.log(request);
+ // console.log(request);
 
   // Messages from content scripts should have sender.tab set
   if (sender.tab) {
@@ -11,10 +11,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (tabId in connections) {
       connections[tabId].postMessage(request);
     } else {
-      console.log("Tab not found in connection list.");
+      // console.log("Tab not found in connection list.");
     }
   } else {
-    console.log("sender.tab not defined.");
+    // console.log("sender.tab not defined.");
   }
   return true;
 });
